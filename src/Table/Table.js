956866f818ce78983d2e62da1397c8a028051af1
Samrith Shankar <samrith.s@media.net>
2018-06-20 01:03:59 +0530

    Removed redundant stuff and used proper row and header renderers

--- src/Table/Table.js
+++ src/Table/Table.js
@@ -1,159 +1,123 @@
-import PropTypes from "prop-types";
-import React, { PureComponent } from "react";
-import { get } from "lodash";
+import PropTypes from 'prop-types';
+import React, { PureComponent } from 'react';
+import { debounce } from 'lodash';
 
-import { HeaderCell, RowCell } from "./Cells";
+import Row, { HeaderRow } from './Rows';
 
-import { ColumnDisplayName } from "./Constants";
-import Errors from "./Errors";
+import { ColumnDisplayName } from './Constants';
+import Errors from './Errors';
 
 export default class Table extends PureComponent {
-  state = {
-    columns: []
-  };
-
-  componentDidMount() {
-    const columns = [];
-    React.Children.forEach(this.props.children, (child, index) => {
-      const { props } = this.validateChild(child);
-      columns.push({ ...props, index });
-    });
-
-    this.setState({ columns });
-  }
-
-  getLeftStyle = cellIndex => {
-    const { children, fixed } = this.props;
-    const { columns } = this.state;
-
-    let left = 0;
-    let zIndex = 1;
-
-    if (fixed) {
-      if (cellIndex === 0) {
-        return { left, zIndex };
-      } else if (cellIndex <= fixed - 1) {
-        columns.forEach(({ width }, index) => {
-          if (index < cellIndex) {
-            left += width;
-          } else {
-            return;
-          }
-        });
-      } else {
-        zIndex = 0;
-        left = "auto";
-      }
-    }
-
-    return { left, zIndex };
-  };
-
-  isLastSticky = index => {
-    const { fixed } = this.props;
-    return fixed && index === fixed - 1;
-  };
-
-  headerRenderer = child => {
-    const { columns } = this.state;
-
-    return (
-      <div className="React-Sticky-Table--Header">
-        {columns.map((column, index) => {
-          const { title, width, dataKey, headerRenderer } = column;
-          const style = { width, ...this.getLeftStyle(index) };
-          return (
-            <HeaderCell
-              title={title}
-              width={width}
-              dataKey={dataKey}
-              index={index}
-              style={style}
-              isLastSticky={this.isLastSticky(index)}
-              renderer={headerRenderer}
-              cellIndex={index}
-              key={`sitcky-table-header-${index}`}
-              onDragEnd={this.handleDragEnd(index)}
-            />
-          );
-        })}
-      </div>
-    );
-  };
-
-  rowRenderer = (rowData, rowIndex) => {
-    const { columns } = this.state;
-
-    return columns.map((column, index) => {
-      const { width, dataKey, rowRenderer } = column;
-      const cellData = get(rowData, dataKey);
-      const style = { width, ...this.getLeftStyle(index) };
-      return (
-        <RowCell
-          dataKey={dataKey}
-          cellData={cellData}
-          rowData={rowData}
-          style={style}
-          isLastSticky={this.isLastSticky(index)}
-          renderer={rowRenderer}
-          cellIndex={index}
-          rowIndex={rowIndex}
-          key={`sitcky-table-row-${rowIndex}-${index}`}
-          onDragEnd={this.handleDragEnd(index)}
-        />
-      );
-    });
-  };
-
-  rowsRenderer = () => {
-    const { data } = this.props;
-
-    return data.map((rowData, index) => {
-      return (
-        <div
-          className="React-Sticky-Table--Row"
-          key={`react-sticky-table-row-${index}`}
-        >
-          {this.rowRenderer(rowData, index)}
-        </div>
-      );
-    });
-  };
-
-  validateChild = child => {
-    if (child.type.displayName === ColumnDisplayName) {
-      return child;
-    } else {
-      throw new Error(Errors.invalidChildren);
-    }
-  };
-
-  handleDragEnd = columnIndex => e => {
-    console.log("drag ended!");
-    const widthDiff = e.clientX - e.target.getBoundingClientRect().left;
-    const newColumns = [...this.state.columns];
-    newColumns[columnIndex] = {
-      ...newColumns[columnIndex],
-      width: newColumns[columnIndex].width + widthDiff
+    state = {
+        columns: []
     };
-    this.setState({
-      columns: newColumns
-    });
-  };
 
-  render() {
-    return (
-      <div className="React-Sticky-Table">
-        {this.headerRenderer()}
-        {this.rowsRenderer()}
-      </div>
-    );
-  }
+    componentDidMount() {
+        const columns = [];
+        React.Children.forEach(this.props.children, (child, index) => {
+            const { props } = this.validateChild(child);
+            columns.push({ ...props, index });
+        });
+
+        this.setState({ columns });
+    }
+
+    getLeftStyle = cellIndex => {
+        const { children, fixed } = this.props;
+        const { columns } = this.state;
+
+        let left = 0;
+        let zIndex = 1;
+
+        if (fixed) {
+            if (cellIndex === 0) {
+                return { left, zIndex };
+            } else if (cellIndex <= fixed - 1) {
+                columns.forEach(({ width }, index) => {
+                    if (index < cellIndex) {
+                        left += width;
+                    } else {
+                        return;
+                    }
+                });
+            } else {
+                zIndex = 0;
+                left = 'auto';
+            }
+        }
+
+        return { left, zIndex };
+    };
+
+    isLastSticky = cellIndex => {
+        const { fixed } = this.props;
+        return fixed && cellIndex === fixed - 1;
+    };
+
+    headerRenderer = child => {
+        const { columns } = this.state;
+
+        return (
+            <HeaderRow
+                columns={columns}
+                rowIndex={0}
+                styleCalculator={this.getLeftStyle}
+                stickyFunction={this.isLastSticky}
+                onDragEnd={this.handleDragEnd}
+            />
+        );
+    };
+
+    bodyRenderer = () => {
+        const { data } = this.props;
+        const { columns } = this.state;
+
+        return data.map((rowData, index) => (
+            <Row
+                columns={columns}
+                rowData={rowData}
+                rowIndex={index + 1}
+                styleCalculator={this.getLeftStyle}
+                stickyFunction={this.isLastSticky}
+                onDragEnd={this.handleDragEnd}
+                key={`sitcky-table-row-${index + 1}`}
+            />
+        ));
+    };
+
+    validateChild = child => {
+        if (child.type.displayName === ColumnDisplayName) {
+            return child;
+        } else {
+            throw new Error(Errors.invalidChildren);
+        }
+    };
+
+    handleDragEnd = columnIndex => e => {
+        const widthDiff = e.clientX - e.target.getBoundingClientRect().left;
+        const newColumns = [...this.state.columns];
+        newColumns[columnIndex] = {
+            ...newColumns[columnIndex],
+            width: newColumns[columnIndex].width + widthDiff
+        };
+        this.setState({
+            columns: newColumns
+        });
+    };
+
+    render() {
+        return (
+            <div className="React-Sticky-Table">
+                {this.headerRenderer()}
+                {this.bodyRenderer()}
+            </div>
+        );
+    }
 }
 
 Table.propTypes = {
-  children: PropTypes.oneOfType([
-    PropTypes.node,
-    PropTypes.arrayOf(PropTypes.node)
-  ]).isRequired
+    children: PropTypes.oneOfType([
+        PropTypes.node,
+        PropTypes.arrayOf(PropTypes.node)
+    ]).isRequired
 };
