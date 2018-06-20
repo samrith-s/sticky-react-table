a3f95bfcccff154163e0e97ac34b7437b29b642e
Samrith Shankar <samrith.s@media.net>
2018-06-20 13:35:19 +0530

    Added custom HTML and specified title

--- nwb.config.js
+++ nwb.config.js
@@ -3,5 +3,11 @@ module.exports = {
   npm: {
     esModules: true,
     umd: false
+  },
+  webpack: {
+    html: {
+      template: 'public/index.html',
+      title: `React Sticky Table - ${process.env.npm_package_version}`
+    }
   }
-}
+};
