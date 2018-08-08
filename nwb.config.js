b0d24df4d02d1fe731ea14bad9e45f0dbb8d9aa5
Samrith Shankar <samrith.s@media.net>
2018-08-08 20:27:50 +0530

    Updated nwb config

--- nwb.config.js
+++ nwb.config.js
@@ -8,6 +8,26 @@ module.exports = {
     html: {
       template: 'public/index.html',
       title: `React Sticky Table - ${process.env.npm_package_version}`
+    },
+    rules: {
+      postcss: {
+        plugins: [require('precss')(), require('autoprefixer')()]
+      },
+      sass: {
+        modules: true,
+        localIdentName: '[hash:base64:5]'
+      }
+    },
+    styles: {
+      sass: [
+        {
+          sass: {
+            modules: true,
+            localIdentName: '[hash:base64:5]',
+            sourceMap: true
+          }
+        }
+      ]
     }
   }
 };
