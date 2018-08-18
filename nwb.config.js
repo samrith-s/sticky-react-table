0e802e2a9852e51a274262c7cc37a1a025533872
Samrith Shankar <samrith.s@media.net>
2018-08-18 06:20:19 +0530

    Updated nwb config

--- nwb.config.js
+++ nwb.config.js
@@ -11,7 +11,7 @@ module.exports = {
     },
     rules: {
       postcss: {
-        plugins: [require('precss')(), require('autoprefixer')()]
+        plugins: [require('precss'), require('autoprefixer')]
       }
     }
   }
