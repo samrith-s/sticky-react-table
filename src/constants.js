f07cd58a3a0bb83bae7337124646a205df42fd29
Samrith Shankar <samrith.s@media.net>
2018-08-18 06:24:47 +0530

    Moved constants file

---
+++ src/constants.js
@@ -0,0 +1,12 @@
+export const ColumnDisplayName = 'Column';
+
+export const cellPropKeys = [
+  'id',
+  'rowData',
+  'cellData',
+  'isChecked',
+  'isCheckbox',
+  'style',
+  'title',
+  'isAllSelected'
+];
